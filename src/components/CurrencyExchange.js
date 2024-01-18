b1d065bda279a18ef46431cf8e98ac0d6304a5b7
Ahmed Elmorabie <46491420+morabe3@users.noreply.github.com>
2024-01-18 22:35:21 +0000

    1 hour mark

--- src/components/CurrencyExchange.js
+++ src/components/CurrencyExchange.js
@@ -1,7 +1,14 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Form } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
 
 export const CurrencyExchange = ({ currencies }) => {
+    const [onClick, setOnClick] = useState(false)
+
+    const calculateResult = ({ Amount, currency }) => {
+        const result = `https://blockchain.info/tobtc?currency=${currency}&value=${Amount}`
+    }
+
     return (
         <Container className='m-5'>
             <Row className='p-2'>
@@ -9,6 +16,20 @@ export const CurrencyExchange = ({ currencies }) => {
                     <span className='exchange-title'>Convert To Bitcoin</span>
                 </Col>
             </Row>
+            <Form className='p-5 exchange-form'>
+                <Form.Group className="mb-3" controlId="amount">
+                    <Form.Label>Amount</Form.Label>
+                    <Form.Control type="number" placeholder="Enter Amount" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="amount">
+                    <Form.Label>Currency</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                        {currencies.map((currency, index) => (
+                            <option key={index} value={currency.symbol}>{currency.symbol}</option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+            </Form>
         </Container>
     )
 
